2fb04c302b911f8c03b724a82b8eeb87f705c4b6
JaimeSoftDev <jaimesoftdev@gmail.com>
2023-04-02 21:32:03 +0200

    Segundo test realizado

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -1,12 +1,15 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
+	public void test_al_crear_un_coche_su_velocidad_es_cero() {
 		Coche nuevoCoche = new Coche();
+		Assert.assertEquals(0, nuevoCoche.velocidad);
 	}
 
 }
